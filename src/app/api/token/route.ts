3383efe0b18e0ceaa95b95cf5280a0f451a75722
sanchai <sanchai20@163.com>
2025-09-04 00:50:07 +0800

    Add JWT secret key to env example and update dependencies

--- src/app/api/token/route.ts
+++ src/app/api/token/route.ts
@@ -1,37 +1,15 @@
 import { auth } from "@/auth";
 import { NextResponse } from "next/server";
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 export async function POST(req: Request) {
-
     let session = await auth();
-    if (!!(session && session.user && session.user.id)) {
+    if (!(session && session.user && session.user.id)) {
         return NextResponse.json(
             { message: "Unauthorized" },
             { status: 401 }
         );
     }
-
-    await jwt.sign("", "")
-
-    const { token } = await req.json();
-    const secret = process.env.TURNSTILE_SECRET_KEY as string;
-    const verifyResponseData = (await fetch(TURNSTILE_VERIFY_URL, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-        },
-        body: `secret=${encodeURIComponent(secret)}&response=${encodeURIComponent(token)}`,
-    }).then((res) => res.json())) as TurnstileServerValidationResponse;
-
-    if (!verifyResponseData.success) {
-        return NextResponse.json(JSON.stringify(verifyResponseData), {
-            status: 400,
-            headers: responseHeaders,
-        });
-    }
-    return NextResponse.json(JSON.stringify(verifyResponseData), {
-        status: 200,
-        headers: responseHeaders,
-    });
+    const token: string = jwt.sign({ user_id: session.user.id }, process.env.JWT_SECRET_KEY as string, { expiresIn: "5s" })
+    return NextResponse.json({ token })
 }
