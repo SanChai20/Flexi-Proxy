faf500d3fc296a0e2831e7987b7d209370f1c645
sanchai <sanchai20@163.com>
2025-09-14 17:27:12 +0800

    Add debug logging and fix unauthorized error message in with-auth middleware

--- src/lib/with-auth.ts
+++ src/lib/with-auth.ts
@@ -9,14 +9,20 @@ type Handler = (req: PayloadRequest, context?: any) => Promise<Response>;
 
 export function withAuth(handler: Handler): Handler {
   return async (req: PayloadRequest, context) => {
+    console.warn(JSON.stringify(req.headers.keys()))
+    console.warn(JSON.stringify(req.headers.values()))
     const authHeader = req.headers.get("Authorization");
-    console.warn(`${req.url} - authHeader: ${authHeader}`);
+    console.warn(`${req.url} - authHeader1: ${authHeader}`);
+    console.warn(`${req.url} - JWT_SECRET_KEY: ${process.env.JWT_SECRET_KEY}`)
+    console.warn(`${req.url} - JWT_ISSUER: ${process.env.JWT_ISSUER}`)
+    console.warn(`${req.url} - JWT_AUDIENCE: ${process.env.JWT_AUDIENCE}`)
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
-      return new Response(JSON.stringify({ error: "Unauthorized 1" }), {
+      return new Response(JSON.stringify({ error: "Unauthorized" }), {
         status: 401,
         headers: { "Content-Type": "application/json" },
       });
     }
+    console.warn(`${req.url} - authHeader2: ${authHeader}`);
     const token = authHeader.split(" ")[1];
     const { payload, error } = await jwtVerify(token);
     if (!payload) {
