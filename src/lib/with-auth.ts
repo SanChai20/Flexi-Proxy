953577d00a2ae9834c2f40a089b2e0b15430ef4a
sanchai <sanchai20@163.com>
2025-09-22 20:47:50 +0800

    feat: add debug logging to authentication process

--- src/lib/with-auth.ts
+++ src/lib/with-auth.ts
@@ -10,6 +10,7 @@ type Handler = (req: AuthRequest, context?: any) => Promise<Response>;
 export function withAuth(handler: Handler): Handler {
   return async (req: AuthRequest, context) => {
     const authHeader = req.headers.get("authorization");
+    console.warn("Auth 1.")
     if (authHeader === null || !authHeader.startsWith("Bearer ")) {
       return new Response(JSON.stringify({ error: "Unauthorized" }), {
         status: 401,
@@ -17,12 +18,14 @@ export function withAuth(handler: Handler): Handler {
       });
     }
     const token = authHeader.split(" ")[1];
+    console.warn("Auth 2.")
     if (process.env.AUTHTOKEN_PREFIX === undefined) {
       return new Response(JSON.stringify({ error: "Internal Error" }), {
         status: 500,
         headers: { "Content-Type": "application/json" },
       });
     }
+    console.warn("Auth 3.")
     const jwtToken: string | null = await redis.get<string>([process.env.AUTHTOKEN_PREFIX, token].join(":"));
     if (jwtToken === null) {
       return new Response(JSON.stringify({ error: "Unauthorized" }), {
@@ -30,6 +33,7 @@ export function withAuth(handler: Handler): Handler {
         headers: { "Content-Type": "application/json" },
       });
     }
+    console.warn("Auth 4.")
     const { payload, error } = await jwtVerify(jwtToken);
     if (payload === undefined) {
       return new Response(JSON.stringify({ error: error }), {
@@ -37,6 +41,7 @@ export function withAuth(handler: Handler): Handler {
         headers: { "Content-Type": "application/json" },
       });
     }
+    console.warn("Auth 5.")
     req.token = token;
     // If authenticated, call the original handler
     return handler(req, context);
