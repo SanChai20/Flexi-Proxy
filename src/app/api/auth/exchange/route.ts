84a1e92e6e3c790ed5f4ec0f63c5438e1990b852
yanzhang <sanchai20@163.com>
2025-09-25 18:30:03 +0800

    feat(api): add provider proxy data handling in auth exchange endpoint
    Extended the auth exchange endpoint to accept and validate provider proxy details (url, status, availability, etc.) in the request body. Added validation for new fields and store them in Redis with expiration using PROVIDER_PREFIX. Also updated environment variable checks to ensure required prefixes are defined.

--- src/app/api/auth/exchange/route.ts
+++ src/app/api/auth/exchange/route.ts
@@ -6,11 +6,32 @@ import { jwtSign } from "@/lib/jwt";
 // POST
 // API: '/api/auth/exchange'
 // Headers: Authorization Bearer <Token>
+// Body: {
+//  [string] url -> provider proxy url
+//  [string] status -> provider proxy status ["unavailable", "spare", "busy", "full"]
+//  [number] ex -> expire seconds
+//  [boolean] adv -> if advanced or not
+//  [string] id -> provider id
+//}
 async function protectedPOST(req: AuthRequest) {
-  if (process.env.AUTHTOKEN_PREFIX === undefined) {
+  if (
+    process.env.AUTHTOKEN_PREFIX === undefined ||
+    process.env.PROVIDER_PREFIX === undefined
+  ) {
     return NextResponse.json({ error: "Internal Error" }, { status: 500 });
   }
   try {
+    const { url, status, ex, adv, id } = await req.json();
+    if (
+      typeof url !== "string" ||
+      typeof status !== "string" ||
+      typeof ex !== "number" ||
+      typeof adv !== "boolean" ||
+      typeof id !== "string"
+    ) {
+      return NextResponse.json({ error: "Missing field" }, { status: 400 });
+    }
+
     const { token: jwtToken, error } = await jwtSign();
     if (jwtToken === undefined) {
       return NextResponse.json({ error }, { status: 500 });
@@ -23,6 +44,11 @@ async function protectedPOST(req: AuthRequest) {
       { ex: 7200 }
     );
     transaction.del([process.env.AUTHTOKEN_PREFIX, req.token].join(":"));
+    transaction.set<{ url: string; status: string; adv: boolean }>(
+      [process.env.PROVIDER_PREFIX, id].join(":"),
+      { url, status, adv },
+      { ex }
+    );
     await transaction.exec();
     return NextResponse.json({ token, expiresIn: 7200 }, { status: 200 });
   } catch (error) {
