1a5b4f18b805ea47570eb2f4f0fd1be6a71b2416
sanchai <sanchai20@163.com>
2025-10-13 22:14:05 +0800

    feat: add PROXY_MODELS_PREFIX environment variable and implement registry API for proxy models

---
+++ src/app/api/[proxyId]/registry/route.ts
@@ -0,0 +1,26 @@
+import { redis } from "@/lib/redis";
+import { NextRequest, NextResponse } from "next/server";
+import { AuthRequest, withAuth } from "@/lib/with-auth";
+
+// POST
+// API: '/api/{proxyId}/registry'
+// Headers: Authorization Bearer <Token>
+// Body: {
+//  [string] provider A -> models set
+//}
+async function protectedPOST(req: AuthRequest, { params }: { params: Promise<{ proxyId: string }> }) {
+    if (process.env.PROXY_MODELS_PREFIX === undefined) {
+        return NextResponse.json({ error: "Internal Error" }, { status: 500 });
+    }
+    try {
+        const { proxyId } = await params;
+        const { models_by_provider } = await req.json();
+        await redis.set<string>([process.env.PROXY_MODELS_PREFIX, proxyId].join(":"), JSON.stringify(models_by_provider));
+        return NextResponse.json({ success: true }, { status: 200 });
+    } catch (error) {
+        console.error("Failed to registry models: ", error);
+        return NextResponse.json({ error: "Internal Error" }, { status: 500 });
+    }
+}
+
+export const POST = withAuth(protectedPOST);
