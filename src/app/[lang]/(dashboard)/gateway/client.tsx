e82af8c1f5ce21e70ac44679d8b8bfd1b29bc006
yanzhang <sanchai20@163.com>
2025-11-21 15:12:54 +0800

    1

--- src/app/[lang]/(dashboard)/gateway/client.tsx
+++ src/app/[lang]/(dashboard)/gateway/client.tsx
@@ -486,7 +486,7 @@ export default function GatewayClient({
 
       {/* Gateway Cards */}
       <div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-3">
           {/* Add Card for Private Gateway */}
           {gatewayType === "private" && (
             <Card
